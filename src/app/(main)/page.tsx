638376289c91e1c2769c31ca4e9228253e88b39a
OhMinSeop <mins5190@gmail.com>
2023-07-31 23:57:56 +0900

    add: 메인 화면 아이템 구성

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { ItemCard } from "~/components/shared/item-card";
 
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <>
+      <div className="py-4"></div>
+      <div className="container grid gap-9 grid-cols-none md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 min-h-screen">
+        {Array.from({ length: 20 }).map((_, i) => {
+          return <ItemCard key={i} />;
+        })}
+      </div>
+    </>
+  );
 }
---
+++ src/components/shared/item-card/item-card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
+export default function ItemCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form></form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
+}
